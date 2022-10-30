848e27382aa23059c47af2781e36e30c084e99c2
coderlei <coderleilei@163.com>
2022-10-30 20:20:40 +0800

    add: dist bundle

---
+++ dist/assets/index.1be40329.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.78610ae4.js";const r={};function c(n,t){return" \u6211\u662F\u6A21\u677F\u9009\u62E9 "}const o=e(r,[["render",c]]);export{o as default};
---
+++ dist/assets/index.528a0d65.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.78610ae4.js";const r={};function c(n,t){return" \u6211\u662Fhome "}const o=e(r,[["render",c]]);export{o as default};
