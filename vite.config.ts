68162678cb729d2cf2786ff4efa228c5628968e1
coderlei <coderleilei@163.com>
2022-11-03 22:12:59 +0800

    fix: bundle path

--- vite.config.ts
+++ vite.config.ts
@@ -20,16 +20,5 @@ export default defineConfig({
       "@": resolve(__dirname, 'src')
     }
   },
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
+  base: './'
 })
