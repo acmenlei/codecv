6a91f0bceeb0977c75810485884790a1e13a5ad8
coderlei <coderleilei@163.com>
2022-11-03 23:10:49 +0800

    split: chunk split

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,16 @@ export default defineConfig({
       "@": resolve(__dirname, 'src')
     }
   },
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
