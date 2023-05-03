6e8cdc62b349ef92352da467635a341886624616
codeleilei <coderleilei@163.com>
2023-05-03 22:27:55 +0800

    refactor: 引入模板方式重构

---
+++ src/templates/modules/13geek/index.ts
@@ -0,0 +1,82 @@
+const content = `
+::: headStart
+
+::: start
+
+![个人头像](https://avatars.githubusercontent.com/u/54731221?v=4)
+
+
+### 带佬
+
+:::
+icon:link [coderlei.netlify.app](https://coderlei.netlify.app)
+icon:github [github.com/acmenlei](https://github.com/acmenlei)
+icon:yuque [yuque.com/xxxxx](https://yuque.com/xxxxx)
+
+:::
+icon:phone 155xxxx9999
+icon:wechat x972761675
+icon:email example@163.com
+
+:::
+## xxx架构师
+
+::: end
+::: headEnd
+## 工作经历
+
+::: start
+#### 腾讯（团队负责人）
+
+:::
+**2019年06月 ～ 至今**
+
+::: end
+-  基于 Web 的 SPA 项目，使用 Vue3 + Typescript 相关生态环境搭建实现在线简历制作平台
+-  使用 \`Vue3\` + \`Typescript\` 编写，使用 \`Vite\` 打包, xxxxxxxxxxxxxxxxxxxxxxxx
+
+::: start
+#### 阿里巴巴 （前端工程师）
+
+:::
+**2017年06月 ～ 2019年06月**
+
+::: end
+负责团队前端公共基础的建设与规划，xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+-  工作中我做了什么，产出了什么样的结果，得到了同事们的一致认可等...
+-  工作中我做了什么，产出了什么样的结果，得到了同事们的一致认可等...
+-  工作中我做了什么，产出了什么样的结果，得到了同事们的一致认可等...
+-  工作中我做了什么，产出了什么样的结果，得到了同事们的一致认可等...
+
+## 项目经历
+::: start
+**Resume Board**
+:::
+**2011年09月 ～ 2015年07月**
+::: end
+**技术栈**：\`TypeScript\` \`Vue3\` \`Vite\` \`pinia\` \`vue-router\`
+**项目简介**：一款 markdown 简历制作工具，可以把你编写的简历转换为 PDF，支持多种模板，编写一套简历内容可适配多种简历模板
+**工作内容**：
+1. 基于 Markdown 的书写方式，简单易上手，且支持多列布局，编写一套 Markdown 可适配所有模版
+2. 根据自身喜好 DIY 简历样式、字体，且提供实时分页预览，可根据比例调整 PDF 预览大小
+3. 智能自动一页，简历内容不足或过多时，可通过动态计算后自动进行均匀拉伸或收缩
+**项目难点**：
+1. 基于原生 Markdown 语法编写的简历排版过于单一，不能实现多列布局
+
+## 教育经历
+::: start
+北京大学 - 软件工程 - 学士学位
+:::
+**2011年09月 ～ 2015年07月**
+::: end
+
+## 自我评价
+在前端领域有过深耕，有管理、组织能力....... ok结束，感觉是口水话的可写可不写吧
+`
+export default {
+  name: '暗黑极客风',
+  primaryColor: '#d1d1d1',
+  primaryBackground: '#17A00B',
+  img: 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/5b816951383e4809b09eeef985afe87b~tplv-k3u1fbpfcp-watermark.image?',
+  content
+}
---
+++ src/templates/modules/5graduation_reexam/index.ts
@@ -0,0 +1,72 @@
+const content = `::: headStart
+::: start
+# 番茄炒鸡蛋
+**性别**：男 ｜ **年龄**：23 ｜ **电话**：11011011011 ｜ **邮箱**：resume@163.com
+:::
+![个人头像](https://avatars.githubusercontent.com/u/54731221?v=4)
+::: end
+::: headEnd
+## icon:practice 报考信息
+::: start
+**报考院校：**xxx大学
+:::
+**报考专业：**金融学（020204）
+:::
+**报考属性：**专硕学位研究生
+::: end
+| 初试科目 | 英语（一） | 政治 | 专业课1 | 专业课2 | 总分 |
+| 初试成绩 | 90 | 80 | 130 | 130 | 430 |
+
+## icon:school 教育经历
+::: start
+### xxx大学
+:::
+2019年9月 - 2023年7月
+::: end
+- GPA: 3.9 / 4.0
+- 国家励志奖学金（2019 - 2020、2020 - 2021、2021 - 2022、2022 - 2023）
+- xxxx学院优秀学生干部（2019 - 2023）
+## icon:head 科研经历
+::: start
+### 基于xxx的研究
+:::
+xxxx年xx月 - xxxx年xx月
+::: end
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+
+::: start
+### 基于xxx的研究
+:::
+xxxx年xx月 - xxxx年xx月
+::: end
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+
+## icon:application 项目经历
+::: start
+### xxxx平台
+:::
+xxxx年xx月 - xxxx年xx月
+::: end
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+## icon:technology 个人技能
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+- xxxxxxxxxxxxxxxxxxxx
+`
+export default {
+  name: '研究生复试模板',
+  primaryColor: '#3A3939',
+  primaryBackground: '#2186D2',
+  img: 'https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7ce5f9d8c876420bbac1a5b0b35ad9f0~tplv-k3u1fbpfcp-watermark.image?',
+  content
+}
