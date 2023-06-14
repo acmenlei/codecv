9ba5c8fa28c8bafb38a458ed7685b55b70002856
codeleilei <coderleilei@163.com>
2023-06-14 21:53:39 +0800

    chore: vite v4.x => v3.x

---
+++ dist/js/syntax-1692abaa.js
@@ -0,0 +1,181 @@
+import{M as a}from"./highlight-98565460.js";import{E as n,r as s,b as l,o as t,c as r,a as c,P as e}from"./@vue-c8b849dd.js";import{_ as p}from"./index-242f51d6.js";import"./index-885f6543.js";import"./config-4240d15d.js";import"./element-plus-5b013558.js";import"./@vueuse-67b6382f.js";import"./lodash-es-742d3967.js";import"./async-validator-8a4f889d.js";import"./@element-plus-6e6d71a8.js";import"./dayjs-e78a62a1.js";import"./aos-17f8bee5.js";import"./@popperjs-c75af06c.js";import"./@ctrl-f8748455.js";import"./markdown-transform-html-ab2f9e2b.js";import"./pinia-9e5c94d8.js";import"./picture-verification-code-aba23b74.js";import"./vue-router-059fda26.js";import"./nprogress-8660dfbd.js";const d=`<h2 style="background:var(--background); color: var(--font-color)">
+<span style="color: var(--font-color); background:(--background)">前言</span>
+</h2>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px; color: var(--font-color)"
+  >如果你想编写一款<strong>比较好的简历排版</strong>，那你可以学习一下下面这些语法，放心，不会花费多少时间.</span
+>
+</p>
+<h2 style="background:var(--background); color: var(--font-color)">
+<span style="color: var(--font-color); background:(--background)">弹性布局</span>
+</h2>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)">它会被渲染为带</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >flex-layout</code
+  ><span style="color: var(--font-color); background:(--background)">类名的弹性盒，而在</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >:::</code
+  ><span style="color: var(--font-color); background:(--background)"
+    >中间的内容将会被渲染为一个弹性元素, 你可以使用它来构造多列布局，如下.</span
+  ></span
+>
+</p>
+<pre
+lang=""
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">::: start</div><div class="tb-code-line">content...</div><div class="tb-code-line">:::</div><div class="tb-code-line">content...</div><div class="tb-code-line">:::</div><div class="tb-code-line">content...</div><div class="tb-code-line">::: end</div></div><span class="tb-pre-lang"></span></pre>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)">上面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >Markdown</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;语法将会被转化为下面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >HTML</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;代码.</span></span
+>
+</p>
+<pre
+lang="HTML"
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">&lt;<span class="tb-hl-tag">div&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">flex-layout</span>"&gt;</div><div class="tb-code-line">&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span class="tb-hl-tag">div&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">flex-layout-item</span>"&gt;content...&lt;/<span class="tb-hl-tag">div</span>&gt;</div><div class="tb-code-line">&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span class="tb-hl-tag">div&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">flex-layout-item</span>"&gt;content...&lt;/<span class="tb-hl-tag">div</span>&gt;</div><div class="tb-code-line">&nbsp;&nbsp;&nbsp;&nbsp;&lt;<span class="tb-hl-tag">div&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">flex-layout-item</span>"&gt;content...&lt;/<span class="tb-hl-tag">div</span>&gt;</div><div class="tb-code-line">&lt;<span class="tb-hl-tag">div</span>&gt;</div></div><span class="tb-pre-lang">HTML</span></pre>
+<h2 style="background:var(--background); color: var(--font-color)">
+<span style="color: var(--font-color); background:(--background)">个人信息布局</span>
+</h2>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)"
+    >如果你想单独对简历头部的个人信息进行排版，那你可以考虑使用&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >Head</code
+  ><span style="color: var(--font-color); background:(--background)"
+    >&nbsp;布局，针对性的对个人信息进行样式设置，和弹性布局差不多，你需要使用&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >:::</code
+  ><span style="color: var(--font-color); background:(--background)"
+    >&nbsp;语法对其进行分割，并给出开始和结束标志.</span
+  ></span
+>
+</p>
+<pre
+lang=""
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">::: headStart</div><div class="tb-code-line">content....</div><div class="tb-code-line">::: headEnd</div></div><span class="tb-pre-lang"></span></pre>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)">上面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >Markdown</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;语法将会被转化为下面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >HTML</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;代码.</span></span
+>
+</p>
+<pre
+lang="HTML"
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">&lt;<span class="tb-hl-tag">div&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">head-layout</span>"&gt;</div><div class="tb-code-line">&nbsp;&nbsp;&nbsp;&nbsp;content....</div><div class="tb-code-line">&lt;<span class="tb-hl-tag">div</span>&gt;</div></div><span class="tb-pre-lang">HTML</span></pre>
+<h2 style="background:var(--background); color: var(--font-color)">
+<span style="color: var(--font-color); background:(--background)">内置图标</span>
+</h2>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px; color: var(--font-color)"
+  >我也内置了一些图标，如果你想使用它，你可以通过以下语法使用，<strong>建议使用空格结尾</strong>.</span
+>
+</p>
+<pre
+lang=""
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">icon:github&nbsp;</div></div><span class="tb-pre-lang"></span></pre>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)">上面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >Markdown</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;语法将会被转化为下面的&nbsp;</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >HTML</code
+  ><span style="color: var(--font-color); background:(--background)">&nbsp;代码.</span></span
+>
+</p>
+<pre
+lang="HTML"
+theme="dark"
+class="tb-pre"
+><div style="width:2.5em" class="tb-code-line-number-bg"></div><div class="tb-code-content"><div class="tb-code-line">&lt;<span class="tb-hl-tag">i&nbsp;</span><span class="tb-hl-attr-name">class</span>="<span class="tb-hl-attr-value">iconfont icon-github</span>"&gt;&lt;/<span class="tb-hl-tag">i</span>&gt;</div></div><span class="tb-pre-lang">HTML</span></pre>
+<h2 style="background:var(--background); color: var(--font-color)">
+<span style="color: var(--font-color); background:(--background)">over</span>
+</h2>
+<p style="background:var(--background); color: var(--font-color)">
+<span style="line-height: 28px"
+  ><span style="color: var(--font-color); background:(--background)"
+    >以上就是该简历所支持的一些特殊语法，其他的语法与 原生</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >Markdown</code
+  ><span style="color: var(--font-color); background:(--background)"
+    >&nbsp;同步，<strong>如果你有不错的想法</strong>，可以向我提出，</span
+  ><code
+    style="
+      font-family: 'Microsoft YaHei Mono', Menlo, Monaco, Consolas, 'Courier New', monospace;
+      color: rgb(229, 61, 14);
+    "
+    >欢迎给这个项目提供不同的简历模板</code
+  ><span style="color: var(--font-color); background:(--background)"
+    >，感谢.&nbsp;<a target="null" href="https://github.com/acmenlei/markdown-resume-to-pdf"
+      >仓库地址</a
+    ></span
+  ></span
+>
+</p>`,i={id:"syntax",class:"flex","data-aos":"fade-right"},b=n({__name:"syntax",setup(g){const o=s();return l(()=>{o.value.innerHTML=d}),(v,u)=>(t(),r("div",i,[c("div",{class:"syntax-helper",ref_key:"helper",ref:o},null,512),e(a,{class:"slider",body:".syntax-helper",style:{width:"300px"}})]))}});const P=p(b,[["__scopeId","data-v-834c2277"]]);export{P as default};
---
+++ dist/js/update-3e51c29f.js
@@ -0,0 +1,10 @@
+import{A as c,B as _}from"./element-plus-5b013558.js";import{E as l,o,c as a,P as d,R as s,F as u,a7 as v,S as f,a as n,W as m,u as h,au as k,av as I}from"./@vue-c8b849dd.js";import{_ as w}from"./index-242f51d6.js";import"./@vueuse-67b6382f.js";import"./lodash-es-742d3967.js";import"./async-validator-8a4f889d.js";import"./@element-plus-6e6d71a8.js";import"./dayjs-e78a62a1.js";import"./aos-17f8bee5.js";import"./@popperjs-c75af06c.js";import"./@ctrl-f8748455.js";import"./pinia-9e5c94d8.js";import"./picture-verification-code-aba23b74.js";import"./vue-router-059fda26.js";import"./nprogress-8660dfbd.js";const y=[{content:`
+      1. 内容模式工具栏新增链接按钮.
+      2. 新增暗黑模板
+      `,timestamp:"2023-05-02",version:"1.4.2"},{content:`
+      1. 内容模式添加工具栏，支持使用UI交互来编写简历内容格式.
+      `,timestamp:"2023-04-26",version:"1.4.1"},{content:`
+      1. 支持两种编辑模式(内容模式 & markdown模式)，解决部分用户markdown语法门槛的问题，但是还是建议使用markdown编辑模式，内容模式目前处于试用阶段，可能出现一些问题(如果出现问题请转到markdown模式进行编辑后再重试).
+      2. 新增上边距调节器，你可以使用该工具调整简历中元素的上边距.
+      3. 调整 UI布局.
+      `,timestamp:"2023-04-23",version:"1.4.0"},{content:"新增UI设计师模板、编辑器中删除线语法、一键重置简历内容到初始状态（不可逆，操作前请注意！）功能，修复已有BUG。",timestamp:"2023-03-13",version:"1.3.4"},{content:"新增多种模板：商务、社招类，上传证件照、自定义主色调、字体颜色功能，并优化自动一页功能，由于每种模板的间距都不太一样，个别模板或许会有微小的误差，可手动进行调整（手动增删部分文字即可）.",timestamp:"2022-11-19",version:"1.3.3"},{content:"新增字体选择、加载动画、文件导入导出、编辑器模式替换输入框，优化简历操作栏排版，修复简历中图片显示的问题.",timestamp:"2022-11-06",version:"1.3.2"},{content:"新增语法小助手，你可以去语法小助手学习编写简历可能会用到的一些特殊语法.",timestamp:"2022-11-03",version:"1.3.1"},{content:"新增实时分页显示、优化导出PDF的方式，提供了原生和动态计算两种导出方式，可以根据自己的偏好选择.",timestamp:"2022-11-02",version:"1.3.0"}],B=t=>(k("data-v-43477f53"),t=t(),I(),t),x={class:"time-line","data-aos":"fade-right"},S=B(()=>n("h3",null,"历史版本",-1)),g=l({__name:"update",setup(t){return(E,U)=>{const i=_,r=c;return o(),a("div",x,[S,d(r,null,{default:s(()=>[(o(!0),a(u,null,v(h(y),(e,p)=>(o(),f(i,{type:"primary",key:p,timestamp:e.timestamp},{default:s(()=>[n("p",null,m(e.version),1),n("p",null,m(e.content),1)]),_:2},1032,["timestamp"]))),128))]),_:1})])}}});const z=w(g,[["__scopeId","data-v-43477f53"]]);export{z as default};
