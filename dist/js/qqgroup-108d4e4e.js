ac064cc78a4614b7ee8688e8f4f77b40db6514d2
codeleilei <coderleilei@163.com>
2023-07-13 14:25:04 +0800

    fix(deploy): 模板代码块背景不显示问题

---
+++ dist/js/qqgroup-108d4e4e.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../jpeg/qqgroup-d324728d.jpeg",import.meta.url).href;export{e as _};
---
+++ dist/js/wechat-61861ca9.js
@@ -0,0 +1,1 @@
+const t=""+new URL("../jpg/wechat-c96a18a0.jpg",import.meta.url).href;export{t as _};
