edf04c0367687d7be67e7216b3fcbffe68202b48
codeleilei <coderleilei@163.com>
2023-07-18 00:18:59 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './permission'
-import '@/common/global.scss'
+import '@/assets/global.scss'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import pinia from '@/store'
 
