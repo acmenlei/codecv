ed3b00a45b1c623b6ed375b7b1bf99e8e730715a
codeleilei <coderleilei@163.com>
2023-10-16 18:37:42 +0800

    feat: 更新通知

--- src/views/template/template.vue
+++ src/views/template/template.vue
@@ -57,21 +57,12 @@ const { flag, close } = useNotification()
   <ToastModal :flag="flag" @close="close">
     <h3 style="margin-bottom: 10px">通知</h3>
     <p style="line-height: 27px">
-      近期反应同学较多，发个通知告知一下，此网址为备用网址，若需体验更多功能请前往主站<del
-        style="color: var(--theme)"
-        ><a
-          target="_blank"
-          href="https://codecv.top"
-          style="color: var(--theme); text-decoration: none"
-        >
-          https://codecv.top</a
-        ></del
-      >(主站正在备案，大概一周左右恢复使用)，可访问临时地址<a
+      近期反应同学较多，发个通知告知一下，此网址为备用网址，若需体验更多功能请前往主站<a
         target="_blank"
-        href="https://wuxiancv.com"
+        href="https://codecv.top"
         style="color: var(--theme); text-decoration: none"
       >
-        https://wuxiancv.com</a
+        https://codecv.top</a
       >
     </p>
     <ol class="" style="margin: 10px 0; padding-left: 20px; line-height: 28px">
